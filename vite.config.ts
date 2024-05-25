1a4f2e0395f95e2ef70e1044319716c2aecac7b4
reworldcreative <reworldcreative@gmail.com>
2024-05-25 16:40:23 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ import svgr from "vite-plugin-svgr";
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd());
   return {
-    base: "/material-landing/",
+    base: "./",
     server: {
       port: 3000,
       strictPort: true,
